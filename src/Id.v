228c9b227648c12e98ce2f3fd4cf840f6605ec52
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2026-06-09 12:24:45 +0300

    feat: add .gitignore, add Id.v
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- src/Id.v
+++ src/Id.v
@@ -64,30 +64,62 @@ Lemma le_gt_id_dec : forall id1 id2 : id, {id1 i<= id2} + {id1 i> id2}.
 Proof. prove_with le_gt_dec. Qed.
 
 Lemma id_eq_dec : forall id1 id2 : id, {id1 = id2} + {id1 <> id2}.
-Proof. admit. Admitted.
+Proof. prove_with Nat.eq_dec. Qed.
 
 Lemma eq_id : forall (T:Type) x (p q:T), (if id_eq_dec x x then p else q) = p.
-Proof. admit. Admitted.
+Proof.
+  intros. destruct (id_eq_dec x x).
+  - reflexivity.
+  - contradiction.
+Qed.
 
 Lemma neq_id : forall (T:Type) x y (p q:T), x <> y -> (if id_eq_dec x y then p else q) = q.
-Proof. admit. Admitted.
+Proof.
+  intros. destruct (id_eq_dec x y).
+  - contradiction.
+  - reflexivity.
+Qed.
 
 Lemma lt_gt_id_false : forall id1 id2 : id,
     id1 i> id2 -> id2 i> id1 -> False.
-Proof. admit. Admitted.
+Proof.
+  intros [n] [m] H1 H2.
+  inversion H1. inversion H2.
+  lia.
+Qed.
 
 Lemma le_gt_id_false : forall id1 id2 : id,
     id2 i<= id1 -> id2 i> id1 -> False.
-Proof. admit. Admitted.
+Proof.
+  intros [n] [m] H1 H2.
+  inversion H1. inversion H2.
+  lia.
+Qed.
 
 Lemma le_lt_eq_id_dec : forall id1 id2 : id, 
     id1 i<= id2 -> {id1 = id2} + {id2 i> id1}.
-Proof. admit. Admitted.
+Proof.
+  intros id1 id2 Hle.
+  destruct (gt_eq_gt_id_dec id1 id2) as [[Hgt|Heq]|Hgt'].
+  - exfalso. eauto using le_gt_id_false.
+  - left. exact Heq.
+  - right. exact Hgt'.
+Qed.
 
 Lemma neq_lt_gt_id_dec : forall id1 id2 : id,
     id1 <> id2 -> {id1 i> id2} + {id2 i> id1}.
-Proof. admit. Admitted.
+Proof.
+  intros [n] [m] H.
+  destruct (lt_eq_lt_dec n m) as [[Hlt|Heq]|Hgt].
+  - right. constructor. assumption.
+  - exfalso. apply H. f_equal. exact Heq.
+  - left. constructor. assumption.
+Qed.
     
 Lemma eq_gt_id_false : forall id1 id2 : id,
     id1 = id2 -> id1 i> id2 -> False.
-Proof. admit. Admitted.
+Proof.
+  intros [n] [m] H1 H2.
+  inversion H1. inversion H2.
+  lia.
+Qed.
